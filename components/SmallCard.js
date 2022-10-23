94e6a141d434d29c5707b196e88676e533c42c14
Dipesh <dipesh@Dipeshs-MacBook-Air.local>
2022-10-23 18:57:44 +0530

    file type changed

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,15 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-function SmallCard({
-  img,
-  location,
-  distance,
-}: {
-  img: "String";
-  location: "String";
-  distance: "String";
-}) {
+function SmallCard({ img, location, distance }) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       {/* left */}
