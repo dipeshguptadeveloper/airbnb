1f1376b511b3c567712a995faf4c715c4e741da3
Dipesh <dipesh@Dipeshs-MacBook-Air.local>
2022-10-29 13:56:46 +0530

    search page  added

---
+++ pages/search.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useRouter } from "next/router";
+import { format } from "date-fns";
+
+function Search() {
+  const router = useRouter();
+  const { location, startDate, endDate, guests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "dd-MMM-yy");
+  const formattedEndDate = format(new Date(endDate), "dd-MMM-yy");
+  const range = `${formattedStartDate} to ${formattedEndDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${guests}`} />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-sm">
+            300+ Stays from {range} for {guests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+          <div className="hidden lg:inline-flex space-x-3 text-gray-800 mb-5 whitespace-nowrap">
+            <p className="button">Cancellation Flexiblity</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More Filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
