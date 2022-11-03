2147fd19029f879f9501c94c66e0e1598dcccd7d
Dipesh <dipesh@Dipeshs-MacBook-Air.local>
2022-11-03 21:43:51 +0530

    search page  added

--- pages/search.js
+++ pages/search.js
@@ -3,9 +3,11 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search({ searchResult }) {
   const router = useRouter();
+  console.log(searchResult);
   const { location, startDate, endDate, guests } = router.query;
 
   const formattedStartDate = format(new Date(startDate), "dd-MMM-yy");
@@ -30,6 +32,23 @@ function Search() {
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
           </div>
+
+          <div className="">
+            {searchResult.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
         </section>
       </main>
       <Footer />
@@ -38,3 +57,14 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResult = await fetch("https://www.jsonkeeper.com/b/ZCEU").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      searchResult,
+    },
+  };
+}
